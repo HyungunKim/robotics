ca2ea64955dd8ab2a12242b816bb24c96dd359d0
Hyunkun Kim <newhyunkun@kookmin.ac.kr>
2021-09-30 15:47:16 +0900

    catkin installed

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/hyun/robotics/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/hyun/robotics/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/hyun/robotics/catkin_ws/devel;/opt/ros/melodic' != '' else []
